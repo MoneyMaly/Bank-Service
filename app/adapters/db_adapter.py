6b80ed0ede47b2e73e3e7818e2a8aaaaf2d7ef84
Tal Danilof <taldl@mta.ac.il>
2022-04-19 17:29:50 +0300

    Added deal existing account check

--- app/adapters/db_adapter.py
+++ app/adapters/db_adapter.py
@@ -10,7 +10,7 @@ db = None
 async def get_bank_accounts_list_by_username(username: str):
     bank_accounts = await db["UsersBankAccounts"].find({"username":username}).to_list(length=5)
     return list(bank_accounts)
-
+# UsersBankAccounts
 async def create_user_bank_account(bank_account: UsersBankAccount):
     try:
         res = await db["UsersBankAccounts"].insert_one(bank_account.__dict__)
@@ -22,6 +22,11 @@ async def delete_user_bank_account(username: str, account_number: str):
     res = await db["UsersBankAccounts"].delete_one({"username": username, "account_number": account_number})
     return res.deleted_count
 
+async def get_users_account(username: str, account_number: str):
+    users_account = await db["UsersBankAccounts"].find_one({"username": username, "account_number": account_number})
+    return users_account
+    
+# BankAccounts   
 async def get_account_monthly_balance(bank_account, year: int, month: int):
     account_monthly_balance = await db["BankAccounts"].find_one({"owner": bank_account["owner"], \
     "ssn": bank_account["ssn"], "account_number":bank_account["account_number"], "year": year, "month": month})
@@ -30,7 +35,7 @@ async def get_account_monthly_balance(bank_account, year: int, month: int):
 async def get_account_monthly_balance_by_number(username: str, account_number: str, year: int, month: int):
     account_monthly_balance = await db["BankAccounts"].find_one({"username": username, "account_number": account_number, "year": year, "month": month})
     return account_monthly_balance
-
+# UsersDeals
 async def insert_users_deal(users_deal: UsersDeal):
     try:
         res = await db["UsersDeals"].insert_one(users_deal.__dict__)
--- app/routers/users_deals.py
+++ app/routers/users_deals.py
@@ -2,7 +2,7 @@ from typing import Optional, List
 
 from fastapi import APIRouter, HTTPException, status, Depends
 
-from app.adapters.db_adapter import insert_users_deal
+from app.adapters.db_adapter import insert_users_deal, get_users_account
 from app.utils.auth_helper import JWTBearer
 from app.models import ExpenceorRevenue, Deal, UsersDeal
 
@@ -13,13 +13,21 @@ credentials_exception = HTTPException(
     detail="Could not validate credentials",
     headers={"WWW-Authenticate": "Bearer"})
 
+account_not_found_exception = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="Users Bank Account doesn't exist",
+    headers={"WWW-Authenticate": "Bearer"})
+
 @router.post("/users/{username}/bankaccounts/{account_number}/deals",status_code=status.HTTP_200_OK, response_model_exclude=['_id'], dependencies=[Depends(JWTBearer())])
 async def add_users_deal_info(username: str, account_number: str, deal_info: Deal):
     if JWTBearer.authenticated_username != username:
         raise credentials_exception
-    deal_info = deal_info.__dict__
-    deal_info["username"] = username
-    deal_info["account_number"] = account_number
-    users_deal =  UsersDeal(**deal_info)
-    is_success = await insert_users_deal(users_deal)
-    return is_success
+    account = await get_users_account(username, account_number)
+    if account:
+        deal_info = deal_info.__dict__
+        deal_info["username"] = username
+        deal_info["account_number"] = account_number
+        users_deal =  UsersDeal(**deal_info)
+        is_success = await insert_users_deal(users_deal)
+        return is_success
+    raise account_not_found_exception
