8575c3831d61ea71d870b6f483c1ea5ceec41f0f
Tal Danilof <taldl@mta.ac.il>
2022-05-03 10:40:57 +0300

    Fix id bug users deals

---
+++ app/models.py
@@ -0,0 +1,52 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+from app.utils.db_helper import PyObjectId, ObjectIdStr
+from datetime import datetime
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+    role: Optional[str] = None
+
+class UserBankAccount(BaseModel):
+    username: Optional[str] = None
+    owner: Optional[str] = None
+    ssn: Optional[str] = None
+    account_number: Optional[str] = None
+class BankAccountBalance(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    ssn: str
+    owner: str
+    account_number:str
+    year: int
+    month: int
+    expenses_and_revenues: list
+
+class ExpenceorRevenue(BaseModel):
+    price: str
+    subject:str
+    date: datetime
+
+class Deal(BaseModel):
+    company: str
+    # can be Communication, TV, insurance
+    sector: str
+    extra_info: dict
+
+class DealDetails(Deal):
+    price : Optional[int] = 0
+    id: Optional[ObjectIdStr] = Field(alias='_id')
+    
+class UserDeal(Deal):
+    username: str
+    account_number: str
+class CompanyMonthlyPrice(BaseModel):
+    company: str
+    price: int
+    year: int
+    month: int 
+    date: datetime
